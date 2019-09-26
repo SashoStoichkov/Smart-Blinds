7f2914f2451b67f757928cd5e9207dc0bfae4451
SashoStoichkov <sashostoichkov@gmail.com>
2019-09-26 17:17:14 +0300

    interface concept

--- main.py
+++ main.py
@@ -1,30 +1,72 @@
 from tkinter import *
-# from tkinter.ttk import *
+from tkinter.ttk import *
 
-window = Tk()
-window.title("Smart Blinds")
-window.geometry('800x480')
-# window.attributes("-fullscreen", True)
+def MainWindow():
+    main_window = Tk()
+    main_window.title("Smart Blinds")
+    # main_window.geometry('800x480')
+    main_window.attributes("-fullscreen", True)
 
-height = 0
+    main_window_label = Label(main_window, text="Welcome to Smart Blinds")
+    main_window_label.pack()
 
-height_l = Label(window, text="Height: " + str(height))
-height_l.grid(column=0, row=0)
+    auto_window_button = Button(main_window, text="Auto", command=AutoWindow)
+    auto_window_button.pack()
 
-def clicked(num):
-    global height
-    if (height + num <= 10 and height + num >= -10):
-        height += num
-        height_l.config(text="Height: " + str(height))
-    else:
-        pass
+    manual_window_button = Button(main_window, text="Manual", command=ManualWindow)
+    manual_window_button.pack()
 
-blinds_up = Button(window, text="+", command=lambda:clicked(1))
-blinds_up.grid(column=1, row=0)
+    main_window.bind("<Escape>", quit)
+    
+    main_window.mainloop()
 
-blinds_down = Button(window, text="-", command=lambda:clicked(-1))
-blinds_down.grid(column=1, row=1)
+def AutoWindow():
+    print("Entering Automatic mode!")
 
-window.bind("<Escape>", quit)
+    auto_window = Tk()
+    auto_window.title("Smart Blinds")
+    # auto_window.geometry('800x480')
+    auto_window.attributes("-fullscreen", True)
 
-window.mainloop()
+    back_to_main_button = Button(auto_window, text="Back to Main", command=auto_window.destroy)
+    back_to_main_button.pack()
+
+def ManualWindow():
+    print("Entering Manual mode!")
+
+    manual_window = Tk()
+    manual_window.title("Smart Blinds")
+    # manual_window.geometry('800x480')
+    manual_window.attributes("-fullscreen", True)
+
+    up_button = Button(manual_window, text="^")
+    up_button.pack()
+
+    down_button = Button(manual_window, text="V")
+    down_button.pack()
+
+    clockwise_rotation_button = Button(manual_window, text="+")
+    clockwise_rotation_button.pack()
+
+    anticlockwise_rotation_button = Button(manual_window, text="-")
+    anticlockwise_rotation_button.pack()
+
+    other_options_button = Button(manual_window, text="More options", command=MoreMaualWindow)
+    other_options_button.pack()
+
+    back_to_main_button = Button(manual_window, text="Back to Main", command=manual_window.destroy)
+    back_to_main_button.pack()
+
+def MoreMaualWindow():
+    print("Entering More Manual mode!")
+
+    more_manual_window = Tk()
+    more_manual_window.title("Smart Blinds")
+    # more_manual_window.geometry('800x480')
+    more_manual_window.attributes("-fullscreen", True)
+
+    back_to_manual_button = Button(more_manual_window, text="Back to Manual", command=more_manual_window.destroy)
+    back_to_manual_button.pack()
+
+if __name__ == "__main__":
+    MainWindow()
